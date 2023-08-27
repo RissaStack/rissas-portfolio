4a603095da057cff7a521d5ffcb00e9edc0a6631
Larissa Stack <larissastack@gmail.com>
2023-08-27 16:22:25 -0700

    pages added

---
+++ src/components/Nav/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Nav = ({pages, currentPage, setCurrentPage}) => {
+    return (
+    <nav className="nav d-flex justify-content-between align-items-center">
+        {pages.map((page) => (
+            <a 
+            href="/#"
+            key={page} 
+            className={`nav-link ${page === currentPage && 'active'}`} 
+            onClick={() => setCurrentPage(page)}
+            >
+                {page}
+            </a>
+        ))}
+    </nav>
+    );
+};
+
+export default Nav;
